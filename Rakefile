176f1f9b9a4b1324135a0b11cfbb11a1980c659a
Jerrold Thompson <jet@whidbey.com>
2014-01-25 19:50:32 -0800

    Fixed Rakefile for auto testing

--- Rakefile
+++ Rakefile
@@ -1,8 +1,8 @@
 require 'rake/testtask'
-task default: "spec"
+task default: "test"
 Rake::TestTask.new do |t|
-   t.libs << "spec"
-   t.pattern = "spec/**/*_spec.rb"
+   t.libs << "test"
+   t.pattern = "test/**/*_spec.rb"
 end
 
 
--- test/spec/models/card_spec.rb
+++ test/spec/models/card_spec.rb
@@ -3,9 +3,13 @@ describe "Card" do
   it "has a suit in the model"do
     card = Card.new(1)
     card.suit.must_equal "Diamond"
-    card.rank.must_equal "3"
     card = Card.new(51)
     card.suit.must_equal "Club"
+  end
+  it "has a rank in the model" do
+    card = Card.new(1)
+    card.rank.must_equal "3"
+    card = Card.new(51)
     card.rank.must_equal "A"
   end
 end
